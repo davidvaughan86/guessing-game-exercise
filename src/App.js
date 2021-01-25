be66b1d798fe90c734e12828a717dff9f343814c
davidvaughan86 <david.vaughan86@gmail.com>
2021-01-24 23:16:09 -0500

    guessing game exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import PickANum from './question'
+
+
+function App() {
+  return (
+    <div className="App">
+      <PickANum />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/question.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+
+export default function PickANum(){
+    
+    return(
+        <h1> pick a number between 1 and 10
+            <div>
+                <button 
+                >Start Game
+                </button>
+                <div>
+                <input type = 'radio' 
+                name='qnum' 
+                value="1"
+                /> 
+                1
+                <input type = 'radio' 
+                name='qnum' 
+                value="2"
+                /> 
+                2
+                <input type = 'radio' 
+                name='qnum' 
+                value="3"
+                /> 
+                3
+                <input type = 'radio' 
+                name='qnum' 
+                value="4"
+                /> 
+                4
+                <input type = 'radio' 
+                name='qnum' 
+                value="5"
+                /> 
+                5
+                <input type = 'radio' 
+                name='qnum' 
+                value="6"
+                /> 
+                6
+                <input type = 'radio' 
+                name='qnum' 
+                value="7"
+                /> 
+                7
+                <input type = 'radio' 
+                name='qnum' 
+                value="8"
+                /> 
+                8
+                <input type = 'radio' 
+                name='qnum' 
+                value="9"
+                /> 
+                9
+                <input type = 'radio' 
+                name='qnum' 
+                value="10"
+                /> 
+                10
+            </div>
+            </div>
+        </h1>
+    
+    )   
+
+}
+// function displayNums() {
+        
+        
+
+//         return(
+//             <div>
+//                 <input type = 'radio' 
+//                 name='qnum' 
+//                 value="1"
+//                 /> 
+//                 1
+                
+//             </div>
+//         )
+    
+//     }
+//     displayNums();
